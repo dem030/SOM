97a641c55cefaa3e9a717659506074985889b5d1
Demetrio Priolo <119853471+dem030@users.noreply.github.com>
2025-12-03 10:04:59 +0100

    completamento script training, pronto al test

--- train_som.py
+++ train_som.py
@@ -1,3 +1,31 @@
 from som import som
-from utils import save_weights, load_weights, plot_quantizaion_error, plot_topology
+from utils import  plot_quantizaion_error, plot_topology
 import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+
+df = pd.read_csv('data/iris.csv', header=None)
+data= df.iloc[:,0:4].values
+
+
+train_data,test_data = train_test_split(data, test_size=0.2,random_state=42)
+som_model = som(grid_height=10,grid_width=10, input_dim=train_data.shape[1], learning_rate=0.1,rad=5, epoc=50)
+print("inizio addestramento")
+som_model.train(train_data, save_to="som_weights.npy")
+print("addestramento completato")
+print("pesi salvati in {save_to}")
+
+train_error = som_model.get_train_errors(train_data)
+test_error = som_model.get_test_errors(test_data)
+
+print("errore medio training set: {train_error: .4f}")
+print("errore medio test set: {test_error: .4f}")
+
+
+plot_quantizaion_error([train_error], [test_error])
+
+plot_topology(som_model.weights)
+
+print("mappa topologica creata")
--- utils.py
+++ utils.py
@@ -13,5 +13,29 @@ def load_weights(filename = 'som_weights.npy'):
     return weights
 
 def plot_quantizaion_error(train_errors,test_errors):
-    
+    plt.figures(figsize=(10,6))
+    plt.plot(train_errors, label='Train Quantization Error')
+    plt.plot(test_errors, label='Test Quantization Error')
+    plt.xlabel('Epochs')
+    plt.ylabel('Quantization Error')
+    plt.title('Quantization Error over Epochs')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
+def plot_topology(weights):
+    h, w, dim = weights.shape
+    #vettorializza i neuroni
+    flat= weights.reshape (h * w, dim)
+
+    pca = PCA(n_components=2)
+    reduced = pca.fit_transform(flat)
+    x = reduced[:, 0].reshape(h, w)
+    y = reduced[:, 1].reshape(h, w)
+
+    plt.figure(figsize=(8,6))
+    plt.title("mappa topologica SOM attraverso PCA")
+    plt.scatter(x,y, c='blue')
+    plt.plot(x,y, 'k-', alpha=0.3)
+    plt.plot(x.T,y.T,'k-', alpha=0.3)
+    plt.show()
